a060e7901a3081297d5fd3d2d4f5e6f85e699c68
Dustin Davis <dmd5073@gmail.com>
2013-03-02 19:43:53 -0600

    NEw

---
+++ bomcreate.py
@@ -0,0 +1,9 @@
+import csv
+
+print " Welcome to the project Bill of Materail Creator. Would like to continue? Y/N"
+
+print "Please Enter Your ENE Project Number:"
+print "Please Enter Your Cleint's Name:"
+print "Please Enter Your Cleint's Workorder Number"
+print "Please Enter Your Design Pressure:"
+print "Please Enter Your Design Factor:"
